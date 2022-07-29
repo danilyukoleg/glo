2767e2253548b45f842feb6a88042fcc658b696f
Oleg Danilyuk <danilyuk_oleg@vk.com>
2022-07-30 01:12:04 +0300

    урок 2 выполнен

--- scripts.js
+++ scripts.js
@@ -1,4 +1,19 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let title = "Типы данных";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 15;
+let rollback = 33;
+let fullPrice = 50000;
+let adaptive = false;
 
-alert("Hello World");
-console.log("Олег Данилюк");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  `Стоимость верстки экранов ${screenPrice}  рублей/ долларов/гривен/юани`
+);
+console.log(
+  `Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`
+);
+console.log(screens.toLowerCase().split(", "));
+console.log(rollback * (fullPrice / 100));
