29ae0a0957c2e46310cbab371aff1256aee95692
Oleg Danilyuk <danilyuk_oleg@vk.com>
2022-08-02 13:58:09 +0300

    выполнил 3 задание

--- scripts.js
+++ scripts.js
@@ -1,9 +1,31 @@
-let title = "Типы данных";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 15;
+"use strict";
+
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
 let rollback = 33;
-let fullPrice = 50000;
-let adaptive = false;
+let adaptive = prompt("Нужен ли адаптив на сайте?");
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+
+let servicePercentPrice = Math.ceil(fullPrice - rollback);
+
+console.log(servicePercentPrice);
+
+if (fullPrice >= 30000) {
+  console.log("Даем скидку в 10%");
+} else if (fullPrice >= 1500 && fullPrice <= 30000) {
+  console.log("Даем скидку в 5%");
+} else if (fullPrice <= 15000) {
+  console.log("Скидка не предусмотрена");
+} else {
+  console.log("Что то пошло не так");
+}
 
 console.log(typeof title);
 console.log(typeof fullPrice);
