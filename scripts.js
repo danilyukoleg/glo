681324e195f74e878d9ee1d2edebb0a59e340d0c
Oleg Danilyuk <danilyuk_oleg@vk.com>
2022-08-04 12:57:44 +0300

    изменил

--- scripts.js
+++ scripts.js
@@ -1,65 +1,63 @@
 "use strict";
-
-let title = prompt("Введите название Вашего проекта...");
-let screens = prompt("Введите типы экранов для разработки");
-let screenPrice = +prompt("Сколько будет стоить данная работа?");
-let rollback = 50;
+let title = prompt("Как называется ваш проект?", "GloAcademy");
+let screens = prompt("Какие типы экранов нужно разработать?", "Декстоп");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", "20000");
+let rollback = 40;
 let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?", "Калькулятор");
+let servicePrice1 = +prompt("Сколько это будет стоить?", "4000");
+let service2 = prompt("Какой дополнительный тип услуги нужен?", "Маркер");
+let servicePrice2 = +prompt("Сколько это будет стоить?", "2000");
 
-let allServicePrices;
 let fullPrice;
 let servicePercentPrice;
+let allServicePrices;
 
-// 1
-const getAllServicePrices = function (price1, price2) {
-  return price1 + price2;
+const getAllServicePrices = function () {
+  return servicePrice1 + servicePrice2;
 };
 
-// 2
-function getFullPrice(screenPrice, allServicePrices) {
+const getFullPrice = function () {
   return screenPrice + allServicePrices;
-}
-
-// 3
-const getTitle = (title) => {
-  return title[0].toUpperCase() + title.slice(1).toLowerCase();
 };
 
-// 4
-const getServicePercentPrices = (fullPrice, rollback) => {
-  return fullPrice - rollback;
+const getTitle = function () {
+  return title[0].trim().toUpperCase();
 };
 
-const getRollbackMessage = (price) => {
+const getServicePercentPrices = function () {
+  return fullPrice - fullPrice * (rollback / 100);
+};
+
+const showTypeof = function (variable) {
+  return console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function (price) {
   if (price >= 30000) {
-    return "Скидка 10%";
-  } else if (price >= 15000) {
-    return "Скидка 5%";
-  } else if (price >= 0) {
+    return "Даем скидку в 10%";
+  } else if (price >= 15000 || price < 30000) {
+    return "Даем скидку в 5%";
+  } else if (price >= 0 || price < 15000) {
     return "Скидка не предусмотрена";
   } else {
     return "Что то пошло не так";
   }
 };
 
-const showTypeOf = (variable) => {
-  console.log(variable, typeof variable);
-};
+allServicePrices = getAllServicePrices();
+fullPrice = getFullPrice();
+servicePercentPrice = getServicePercentPrices();
 
-// 5
-showTypeOf(title);
-showTypeOf(screenPrice);
-showTypeOf(adaptive);
+showTypeof(title);
+showTypeof(adaptive);
+showTypeof(fullPrice);
 
-getAllServicePrices(servicePrice1, servicePrice2);
-getFullPrice(screenPrice, allServicePrices);
-getServicePercentPrices(fullPrice, rollback);
-
-console.log(getTitle(title));
-console.log("screens: ", screens);
+console.log(screens.length);
+console.log(
+  `Стоимость верстки экранов ${screenPrice} рублей/ долларов/гривен/юани 
+  Стоимость разработки сайта ${fullPrice} рублей/ долларов/гривен/юани`
+);
+console.log(screens.toLowerCase().split(", "));
 console.log(getRollbackMessage(fullPrice));
-console.log(servicePercentPrice);
+console.log(Math.ceil(servicePercentPrice));
